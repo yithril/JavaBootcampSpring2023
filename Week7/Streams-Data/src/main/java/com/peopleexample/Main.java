302db68caa2a78bd8581d602083538a0a7efda49
Jonathan Hop <yithril@gmail.com>
2023-05-23 21:22:51 -0400

    People generator

---
+++ Week7/Streams-Data/src/main/java/com/peopleexample/Main.java
@@ -0,0 +1,11 @@
+package com.peopleexample;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        //Example usage, generate 10000 random people
+        List<Person> personList = PersonGenerator.generateRandomPeople(10000);
+
+    }
+}
