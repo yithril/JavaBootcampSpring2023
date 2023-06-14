05d63c0645cc9695dff2adae5abe1f52515702cd
Jonathan Hop <yithril@gmail.com>
2023-06-14 15:25:02 -0400

    race controller finished

---
+++ Week9/ZorkPartDeux/src/main/java/com/zork/controllers/RaceController.java
@@ -0,0 +1,26 @@
+package com.zork.controllers;
+
+import com.zork.models.Race;
+import com.zork.models.dao.RaceDAO;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+public class RaceController {
+    private DataSource dataSource;
+    private RaceDAO raceDAO;
+
+    public RaceController(DataSource dataSource){
+        this.dataSource = dataSource;
+        this.raceDAO = new RaceDAO(dataSource);
+    }
+
+    public List<Race> getAllRaces(){
+        return this.raceDAO.getAllRaces();
+    }
+
+    public Optional<Race> getRaceByID(int id){
+        return this.raceDAO.getRaceByID(id);
+    }
+}
