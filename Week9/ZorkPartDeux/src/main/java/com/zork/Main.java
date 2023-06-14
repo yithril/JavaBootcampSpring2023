62dca38a42df4b7cbb014a7256baac6e4df0a9e5
Jonathan Hop <yithril@gmail.com>
2023-06-14 15:52:53 -0400

    hooked up to the controler and displaying

--- Week9/ZorkPartDeux/src/main/java/com/zork/Main.java
+++ Week9/ZorkPartDeux/src/main/java/com/zork/Main.java
@@ -1,11 +1,38 @@
 package com.zork;
 
+import com.zork.controllers.RaceController;
 import com.zork.models.Player;
+import com.zork.view.PlayerCreation;
+import org.apache.commons.dbcp2.BasicDataSource;
 
+import javax.sql.DataSource;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        if(args.length != 2){
+            System.out.println("Username and password is required");
+            System.exit(1);
+        }
 
+        String userName = args[0];
+        String password = args[1];
+
+        DataSource source = getDataSource(userName, password);
+        RaceController raceController = new RaceController(source);
+
+        PlayerCreation playerCreation = new PlayerCreation(raceController);
+
+        playerCreation.characterCreateScreen();
+    }
+
+    private static BasicDataSource getDataSource(String userName, String password){
+        BasicDataSource dataSource = new BasicDataSource();
+
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+        dataSource.setUrl("jdbc:mysql://localhost:3306/zork");
+
+        return dataSource;
     }
 }
